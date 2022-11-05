bbaf48d022b6a7ec38f8e4e793ea61eedc73a926
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-11-05 16:33:22 -0400

    main page

--- ai-website-builder-react/src/App.js
+++ ai-website-builder-react/src/App.js
@@ -2,25 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import ResultPage from './pages/ResultPage';
 import { Configuration, OpenAIApi } from "openai";
+/*
 const configuration = new Configuration({
     organization: "org-r8hqgbSkNNa8LHOzUpsJ91Ua",
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 const response = await openai.listEngines();
-
+*/
 function App() {
   return (
      <div className='mainTitleMenu'>
        <div className='centeronly' >
          <h1 className='bigTitleText centeronly' style={{marginTop:"5%", marginBottom:"0"}}>CREATE AI</h1>
          <h3 className='bigTitleText centeronly' style={{marginTop:"0"}}>GENERATED UI</h3>
-         <div className='centeronly' style={{marginBottom:"2%"}}>SketchUI generates beautiful UI components and websites</div>
+         <div className='centeronly' style={{marginBottom:"2%"}}>SketchUI generates beautiful UI components and website elements from your drawings.</div>
        </div>
        <input
               type="text"
-              placeholder="Enter your username"
-              className='loginInput'/>
+              placeholder="Describe your component here"
+              className='login'/>
        <button className='button-google'>Submit</button>
        <br></br>
      <ResultPage/>
