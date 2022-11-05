cf17f2c3133de073b5bea8cf68bf1db33c5a9c0f
matt <mat@unc.edu>
2022-11-05 15:15:33 -0400

    small changes

--- ai-website-builder-react/src/index.js
+++ ai-website-builder-react/src/index.js
@@ -4,10 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import ResultPage from './pages/ResultPage';
-// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-// import ViewLife from './pages/ViewLife';
-// import fs from 'fs'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,11 +17,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-// serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
