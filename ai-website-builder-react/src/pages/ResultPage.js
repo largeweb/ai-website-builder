0e36d86fc9cea9322729069c3a695c919a2b3395
matt <mat@unc.edu>
2022-11-05 15:14:15 -0400

    small changes

---
+++ ai-website-builder-react/src/pages/ResultPage.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+function ResultPage() {
+  return (
+    <div className="App">
+        <p>TEST</p>
+    </div>
+  );
+}
+
+export default ResultPage;
