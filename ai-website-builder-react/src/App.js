c8bbee1111deb6ca14dc5502199d5fc0aa87af3b
Gaurav Rao <69742533+Gauravmrao@users.noreply.github.com>
2022-11-05 17:19:39 -0400

    fixed the app.css

--- ai-website-builder-react/src/App.js
+++ ai-website-builder-react/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import ResultPage from './pages/ResultPage';
-import { Configuration, OpenAIApi } from "openai";
+//import { Configuration, OpenAIApi } from "openai";
 /*
 const configuration = new Configuration({
     organization: "org-r8hqgbSkNNa8LHOzUpsJ91Ua",
@@ -12,23 +12,32 @@ const response = await openai.listEngines();
 */
 function App() {
   return (
-     <div className='mainTitleMenu' style={{paddingLeft: "15%", paddingRight:"15%", paddingTop: "1%"}}>
-       <h1 className='midTitleText' style={{marginTop:"0%"}}>SketchUI</h1> 
-       <div className='centeronly' >
-         <h1 className='bigTitleText centeronly' style={{marginTop:"5%", marginBottom:"0%"}}>CREATE AI</h1>
-         <h3 className='bigTitleText centeronly' style={{marginTop:"0", marginBottom:"0"}}>GENERATED UI</h3>
-         <div className='smallTitleText centeronly' style={{marginBottom:"1%"}}>SketchUI generates beautiful UI components and website elements</div>
-         <div className='smallTitleText centeronly' style={{marginBottom:"2%"}}>from your drawings - free forever.</div>
-         <br></br>
-       </div>
-       <input
-              type="text"
-              placeholder="Describe your component here"
-              className='login'/>
-       <button className='button-google'>Submit</button>
-       <br></br>
-     <ResultPage/>
-     </div>
+    <div>
+
+      <div className='mainTitleMenu' style={{paddingLeft: "15%", paddingRight:"15%", paddingTop: "1%"}}>
+        <h1 className='midTitleText' style={{marginTop:"0%"}}>SketchUI</h1> 
+        <div className='centeronly' >
+          <h1 className='bigTitleText centeronly' style={{marginTop:"5%", marginBottom:"0%"}}>CREATE AI</h1>
+          <h3 className='bigTitleText centeronly' style={{marginTop:"0", marginBottom:"0"}}>GENERATED UI</h3>
+          <div className='smallTitleText centeronly' style={{marginBottom:"1%"}}>SketchUI generates beautiful UI components and website elements</div>
+          <div className='smallTitleText centeronly' style={{marginBottom:"2%"}}>from your drawings - free forever.</div>
+          <br></br>
+        </div>
+      </div>
+
+      <div id="rightBlock">
+        <input
+          type="text"
+          placeholder="Describe your component here"
+          className='login'/>
+        <button className='button-google'>Submit</button>
+        <br></br>
+      </div>
+
+      
+      <ResultPage/>
+
+    </div>
   );
 }
 
