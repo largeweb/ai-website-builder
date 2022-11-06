f121dd03721766d93049335d797f52f25525ff84
matt <mat@unc.edu>
2022-11-06 01:05:06 -0400

    small changes

--- ai-website-builder-react/server.js
+++ ai-website-builder-react/server.js
@@ -4,10 +4,14 @@ const port = process.env.PORT || 5000; //Line 3
 const exec = require('child_process').exec;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { count } = require('console');
 
 require('dotenv').config();
 
 const OPENAI_API_KEY = process.env.OPENAI_KEY;
+
+let outputjsonstring = "";
+let grcount = 1;
 
 // This displays message that the server running and listening to specified port
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
@@ -21,30 +25,69 @@ app.get('/', (req, res) => { //Line 9
 //MIDDLEWARE
 // app.use(urlencoded({extended: true}))
 // app.use(upload.array())
-app.use(cors())
+app.use(cors({origin:'*'}))
 app.use(express.json())
 app.use(bodyParser.json());
 
 
 app.post('/fetch_openapi/', (req, res) => {
-  input = req.body.inputtext;
+  input = '"' + req.body.inputtext + '"';
+  grcount++;
   console.log("TESTING THIS WITH INPUT " + input);
   let scmd = './scripts/openai-request ' + OPENAI_API_KEY + " " + input;
   console.log(scmd);
   console.log("TESTING THIS V2")
   let output = "";
   exec(scmd, (err, stdout, stderr) => {
-        setTimeout(function(){
+        // setTimeout(function(){
                 console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
+                // console.log('stderr: ' + stderr);
                 if (err !== null) {
                         console.log('exec error: ' + err);
                 }
                 console.log("FINISHED:")
                 console.log(stdout)
-                output = stdout;
-                res.json(JSON.parse(output));
-        }, 5000)
+                outputjsonstring = stdout;
+
+
+                // THIS WILL PUT RESPONSE DATA IN THE RESULT PAGE
+                // exec('cat ./src/pages/result-first-half > ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                //         console.log("output is: " + outputjsonstring)
+                // })
+                // exec('echo "' + JSON.parse(outputjsonstring).choices[0].text + '" >> ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                //         console.log("output is: " + outputjsonstring)
+                // })
+                // exec('cat ./src/pages/result-second-half >> ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                //         console.log("output is: " + outputjsonstring)
+                // })
+
+                // THIS WILL PUT BUTTON WITH RESPONSE DATA IN THE RESULT CSS
+                uniqueButtonId = "testbtn" + grcount;
+                exec('cat ./src/pages/result-first-half > ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                        console.log("output is: " + outputjsonstring)
+                })
+                exec('echo "' + "<button id='" + uniqueButtonId + "'>Test Button</button>" + '" >> ./src/pages/result-data', (err, stdout, stderr) => {
+                        console.log("output is: " + outputjsonstring)
+                })
+                exec('cat ./src/pages/result-data >> ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                        console.log("output is: " + outputjsonstring)
+                })
+                exec('cat ./src/pages/result-second-half >> ./src/pages/ResultPage.js', (err, stdout, stderr) => {
+                        console.log("output is: " + outputjsonstring)
+                })
+                exec('echo " #'+ uniqueButtonId + ' {' + JSON.parse(outputjsonstring).choices[0].text + '" >> ./src/pages/result.css', (err, stdout, stderr) => {
+                        console.log("output is: " + outputjsonstring)
+                })
+                // exec('echo "' + JSON.parse(outputjsonstring).choices[0].text + '" >> ./src/pages/result.css', (err, stdout, stderr) => {
+                //         console.log("output is: " + outputjsonstring)
+                // })
+
+                res.json(JSON.parse(outputjsonstring));
+
+
+
+                // res.json(JSON.parse('{"output":'+`${output}`+'}'));
+        // }, 500)
   });
 //   res.json({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 })
