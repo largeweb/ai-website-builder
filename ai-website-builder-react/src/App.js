4150d44d7454c7ee365fbf6c4343f2a4420c3fa1
Ernest Ermongkonchai <erneste@email.unc.edu>
2022-11-06 01:33:51 -0400

    UI

--- ai-website-builder-react/src/App.js
+++ ai-website-builder-react/src/App.js
@@ -55,30 +55,37 @@ function App() {
 
       
       <div className='mainTitleMenu'> 
+        <div>
+          {/* {<img src='https://img.freepik.com/premium-vector/pixel-art-old-computer-with-retro-8-bit-platform-game-vector-icon-8bit-game-white-background_360488-1145.jpg?w=2000' className='titleArt'></img>} */}
+          <img src='https://static.vecteezy.com/system/resources/previews/002/206/017/original/web-programming-icon-free-vector.jpg' className='titleArt'></img>
+        </div>
         <div className='centeronly' >
           <h1 className='bigTitleText centeronly' style={{marginTop:"80px", marginBottom:"0%"}}>Automate</h1>
           <h2 className='bigTitleText centeronly' style={{marginTop:"-30px", marginBottom:"0px"}}>Your</h2>
           <h3 className='bigTitleText centeronly' style={{marginTop:"-30px", marginBottom:"0px"}}>User</h3>
-          <h4 className='bigTitleText centeronly' style={{marginTop:"-30px", marginBottom:"0px"}}>Interface</h4>
-          <div className='smallTitleText centeronly' style={{marginBottom:"1%"}}>SketchUI generates beautiful</div>
-          <div className='smallTitleText centeronly' style={{marginBottom:"1%"}}>UI components and website elements</div>
-          <div className='smallTitleText centeronly' style={{marginBottom:"2%"}}>from your drawings - free forever.</div>
+          <h4 className='bigTitleText centeronly' style={{marginTop:"-30px", marginBottom:"10px"}}>Interface</h4>
+          <div className='smallTitleText centeronly' style={{marginBottom:"5px"}}>SketchUI generates beautiful</div>
+          <div className='smallTitleText centeronly' style={{marginBottom:"5px"}}>UI components and website elements</div>
+          <div className='smallTitleText centeronly' style={{marginBottom:"2%"}}>from your drawings or words.</div>
           <br></br>
         </div>
       </div>
 
-      <div className='rightBlock'>
-        <div className='upload'>
-          <h2>Add Image:</h2>
 
-          <div className='imageBox'>
-          <input type="file" onChange={handleChange} />
-          <img className = "picture" src={file} />
-          </div>
-        </div>
-        <div class="fancy-box-with-button">
-          <input type="text" placeholder='Describe your component here' value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
+      <div>
+        <div class="fancy-box-with-button centeronly">
+          <input type="text" placeholder='   Describe your component here' value={userInput} onChange={(e) => setUserInput(e.target.value)}/>
           {<button onClick={(e) => fetchFromOpenAPI()}>Submit</button>}
+        </div>
+        <br></br>
+
+
+        <div className='midTitleText centeronly'>Or Add Your Image</div>
+        <br></br>
+
+        <div className='imageBox centeronly'>
+          <input className='' type="file" onChange={handleChange} />
+          <img className = "picture" src={file} />
         </div>
       </div>
 
